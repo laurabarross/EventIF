f3dc66a3963aed8f58dd4e4842ac88f0aeb53ff2
laurabarross <101750445+laurabarross@users.noreply.github.com>
2024-11-27 10:19:40 -0300

    Update admin.py

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,20 +1,25 @@
+"""
+URL configuration for eventif project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.utils.timezone import now
-from subscriptions.models import Subscription
+from django.urls import path, include
+from core.views import home
 
-
-class SubscriptionModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone', 'cpf',
-                    'created_at', 'subscribed_today')
-    date_hierarchy = 'created_at'
-    search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')
-    list_filter = ('created_at',)
-
-    def subscribed_today(self, obj):
-        return obj.created_at.date() == now().date()
-
-    subscribed_today.short_description = 'Inscrito hoje?'
-    subscribed_today.boolean = True
-
-
-admin.site.register(Subscription, SubscriptionModelAdmin)
+urlpatterns = [
+    path('', home, name='home'),
+    path('inscricao/', include('subscriptions.urls')),
+    path('admin/', admin.site.urls),
+]
