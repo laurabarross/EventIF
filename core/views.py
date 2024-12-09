4a24b58f4c4574aac58c51ec8e65126a02d74dc6
laurabarross <101750445+laurabarross@users.noreply.github.com>
2024-12-09 15:06:04 -0300

    Finalizando o projeto

--- core/views.py
+++ core/views.py
@@ -4,13 +4,11 @@ from core.models import Speaker
 
 
 def home(request):
-    speakers = [
-        {'name': 'Grace Hopper', 'photo': 'https://abre.ai/hopper-pic'},
-        {'name': 'Alan Turing', 'photo': 'https://abre.ai/turing-pic'}
-    ]
+    speakers = Speaker.objects.all()
     return render(request, 'index.html', {'speakers': speakers})
 
 
 def speaker_detail(request, slug):
     speaker = get_object_or_404(Speaker, slug=slug)
     return render(request, 'core/speaker_detail.html', {'speaker': speaker})
+    
